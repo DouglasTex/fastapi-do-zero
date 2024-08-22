c3718edcb0a8ed3f7ccc8a367bb6be11ee53caa7
Douglas-Cardins <dvac@softex.ufpe.br>
2024-08-22 15:16:53 -0300

    lesson 2: added exercise and test

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -10,3 +11,20 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Aoba'}
+
+
+@app.get('/aoba', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_http():
+    return """
+        <!DOCTYPE html>
+        <html lang="pt-BR">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Página teste</title>
+        </head>
+        <body>
+            <p>Aoba</p>
+        </body>
+        </html>
+        """  # noqa: E501
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -12,3 +12,24 @@ def test_read_root_must_return_ok_and_aoba():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Aoba'}
+
+
+def test_html_end_point_return_ok_and_html_aoba():
+    client = TestClient(app)
+
+    response = client.get('/aoba')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == """
+        <!DOCTYPE html>
+        <html lang="pt-BR">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Página teste</title>
+        </head>
+        <body>
+            <p>Aoba</p>
+        </body>
+        </html>
+        """  # noqa: E501
